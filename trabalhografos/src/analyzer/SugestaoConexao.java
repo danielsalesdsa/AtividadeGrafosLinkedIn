3be1ba028f6a19b3f451870653f98e61d5b18480
Carlos Henrique <167443059+carl0s37@users.noreply.github.com>
2026-06-28 22:37:17 -0300

    Add files via upload

---
+++ trabalhografos/src/analyzer/SugestaoConexao.java
@@ -0,0 +1,47 @@
+
+package analyzer;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class SugestaoConexao {
+
+    private final String nome;
+    private final int amigosEmComum;
+    private final List<String> nomesAmigosEmComum;
+    private final String caminhoExemplo;
+
+
+    public SugestaoConexao(String nome, int amigosEmComum, List<String> nomesAmigosEmComum, String caminhoExemplo) {
+        this.nome = nome;
+        this.amigosEmComum = amigosEmComum;
+        this.nomesAmigosEmComum = new ArrayList<>(nomesAmigosEmComum);
+        this.caminhoExemplo = caminhoExemplo;
+    }
+
+
+    public String getNome() {
+        return nome;
+    }
+
+
+    public int getAmigosEmComum() {
+        return amigosEmComum;
+    }
+
+
+    public List<String> getNomesAmigosEmComum() {
+        return Collections.unmodifiableList(nomesAmigosEmComum);
+    }
+
+
+    public String getCaminhoExemplo() {
+        return caminhoExemplo;
+    }
+
+    @Override
+    public String toString() {
+        return nome + " (" + amigosEmComum + " em comum: " + String.join(", ", nomesAmigosEmComum) + ")";
+    }
+}
